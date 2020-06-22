683f6183b4554fde4ff9046ec13751173c17d6df
Shchuchinov.Ruslan <Ruslan.Shchuchinov@infotecs.ru>
2020-06-22 13:01:59 +0300

    KoroutineLibs -> CoroutineLibs

--- buildSrc/src/main/java/CoroutineLibs.kt
+++ buildSrc/src/main/java/CoroutineLibs.kt
@@ -1,4 +1,4 @@
-object KoroutineLibs {
+object CoroutineLibs {
     private const val version = "1.3.2"
     const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
     const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
