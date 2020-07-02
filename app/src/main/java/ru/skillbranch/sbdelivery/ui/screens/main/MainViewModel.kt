efc259edfd99743c880cecc137eceeb6f157201a
ruslanjava <ruslan.java@gmail.com>
2020-07-02 23:49:22 +0300

    Более простой способ обновления списков блюд - через LiveData DAO

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/screens/main/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/screens/main/MainViewModel.kt
@@ -1,13 +1,8 @@
 package ru.skillbranch.sbdelivery.ui.screens.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import ru.skillbranch.sbdelivery.application.SbDeliveryApplication
 import ru.skillbranch.sbdelivery.http.HttpClient
@@ -33,28 +28,25 @@ internal class MainViewModel : ViewModel() {
 
     @ExperimentalCoroutinesApi
     fun popularDishes(): LiveData<List<Dish>> {
-        popularDishes.addSource(dishDao.getPopularDishes(), Observer { popularDishes.postValue(it) })
-        return popularDishes
+        return dishDao.getPopularDishes()
     }
 
     @ExperimentalCoroutinesApi
     fun recommendedDishes(): LiveData<List<Dish>> {
-        recommendedDishes.addSource(dishDao.getRecommendedDishes(), Observer { recommendedDishes.postValue(it) })
+        recommendedDishes.addSource(dishDao.getRecommendedDishes(), Observer {
+            recommendedDishes.postValue(it)
+        })
         viewModelScope.launch(Dispatchers.IO) {
             // затем отдаем обновленный вариант
             val ids = HttpClient.getRecommendedIds()
             dishDao.updateRecommendedDishes(ids)
-            MainScope().launch {
-                recommendedDishes.addSource(dishDao.getRecommendedDishes(), Observer { recommendedDishes.postValue(it) })
-            }
         }
         return recommendedDishes
     }
 
     @ExperimentalCoroutinesApi
     fun bestDishes(): LiveData<List<Dish>> {
-        bestDishes.addSource(dishDao.getBestDishes(), Observer { bestDishes.postValue(it) })
-        return bestDishes
+        return dishDao.getBestDishes()
     }
 
     fun addedDishes(): LiveData<Dish?> {
@@ -79,12 +71,8 @@ internal class MainViewModel : ViewModel() {
 
     fun handleFavoriteClick(dish: Dish) {
         viewModelScope.launch(Dispatchers.IO) {
-            dishDao.changeFavoriteState(dish.id)
-            MainScope().launch {
-                popularDishes.addSource(dishDao.getPopularDishes(), Observer { popularDishes.postValue(it) })
-                recommendedDishes.addSource(dishDao.getRecommendedDishes(), Observer { recommendedDishes.postValue(it) })
-                bestDishes.addSource(dishDao.getBestDishes(), Observer { bestDishes.postValue(it) })
-            }
+            val dishId = dish.id
+            dishDao.changeFavoriteState(dishId)
         }
     }
 
